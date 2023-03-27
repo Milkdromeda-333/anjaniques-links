ccdcdddd9cbfc5236ce0a4396c0dcbacad861b54
Anjanique <anjaniquemackey@gmail.com>
2023-03-27 02:59:42 -0700

    attempt to fix image path for deployment

--- index.js
+++
@@ -1,6 +0,0 @@
-const credits = document.querySelector(".container--photo-credits");
-const mobileScreen = 720;
-
-document.addEventListener("resize", () => {
-    credits.textContent = "Background Photo by Anna Shvets: https://www.pexels.com/photo/ocean-during-sunset-4014919/";
-});
